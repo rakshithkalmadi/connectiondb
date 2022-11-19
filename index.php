ad15908dd60f9ab8e05fbdb75bb3af0fabb3c65e
rakshithkalmadi <rakshithkalmadi@gmail.com>
2022-11-19 11:52:21 +0530

    mm

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 $conn = mysqli_connect('localhost', 'root', '', 'projectdb') or die("Unable to connect to database");
 echo "Great Work!";
+
+
+$sql = 'SELECT id, name, ex FROM exampletbl ORDER BY id';
+
+$result = mysqli_query($conn, $sql);
+
+$final = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+print_r($final);
 ?>
 <html>
     <body>
