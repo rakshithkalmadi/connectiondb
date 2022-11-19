8198ba91ee5c98af3da6cf90528f53c0310d5e38
rakshithkalmadi <rakshithkalmadi@gmail.com>
2022-11-19 11:48:54 +0530

    n

--- index.php
+++ index.php
@@ -1,24 +1,10 @@
 <?php
-$conn = mysqli_connect('localhost', 'username', 'password', 'dbname') or die("Unable to connect to database");
+$conn = mysqli_connect('localhost', 'root', '', 'projectdb') or die("Unable to connect to database");
 echo "Great Work!";
-
-//sql staments
-$sql = 'SELECT id, name, ex FROM table_name ORDER BY id';
-
-//print out the result
-$result = mysqli_query($conn, $sql);
-
-
-//rep result in the form of an array
-$final = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-
-//free result from memory
-print_r($final);
 ?>
 <html>
     <body>
-        <p>Hello</p>
-
+        <p>My first paragraph.</p>
+        <h1>Hello World !</h1>
     </body>
 </html>
